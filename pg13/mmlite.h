7aa2ef3ac4fb9d343541db4b009498fc07e5b993
Jesper St John <admin@zonezero.se>
2021-03-12 22:57:04 +0100

    Add pg13 version of pgoutput plugin.

---
+++ pg13/mmlite.h
@@ -0,0 +1,29 @@
+/*-------------------------------------------------------------------------
+ *
+ * mmlite.h
+ *		Logical Replication output plugin for lightweight
+ *      multimaster environments
+ *
+ * IDENTIFICATION
+ *		mmlite.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef MMLITE_H
+#define MMLITE_H
+
+#include "nodes/pg_list.h"
+
+typedef struct MMLiteData
+{
+	MemoryContext context;		/* private memory context for transient
+								 * allocations */
+
+	/* client info */
+	uint32		protocol_version;
+
+	List	   *publication_names;
+	List	   *publications;
+} MMLiteData;
+
+#endif							/* MMLITE_H */
